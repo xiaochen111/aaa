980f16068d0dfe059a7a30a30d33e4c1db348f3e
chenhaibo <chb0413@163.com>
2019-09-26 09:58:13 +0800

    添加vue文件

---
+++ navbar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <van-nav-bar :title="title" left-arrow fixed  @click-left="onClickLeft">
+            <div slot="right">
+                <slot></slot>
+            </div>
+        </van-nav-bar>
+    </div>
+</template>
+
+<script>
+    import { NavBar} from 'vant';
+    export default {
+        components:{
+            [NavBar.name]:NavBar,
+        },
+        props:['title','url'],
+        // props:{
+        //     title:{
+        //         type:String,
+        //         default:''
+        //     },
+        //     url:{
+        //         type:String,
+        //         default:''
+        //     }
+        // },
+        methods:{
+            onClickLeft(){
+                if(this.url){
+                    this.$router.push({path:this.url,})
+                }else{
+                    if(history.length>1){
+                        history.go(-1);
+                    }else{
+                        this.$router.push({path:'/platform',})
+                    }
+                }
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
